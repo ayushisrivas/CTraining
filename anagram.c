721cca75e1531a4262d8ac8b181e0da7bde89318
ayushisrivas <user13@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-06-26 03:38:38 +0000

    Corrected anagram using strcmp

--- anagram.c
+++ anagram.c
@@ -3,37 +3,18 @@
 int main()
 {
 	char str[20],str1[20];
-	printf("Enter string one: ");
+	printf("Enter string one: \n");
 	fgets(str,20,stdin);
-	printf("Enter string two: ");
+	printf("Enter string two: \n");
 	fgets(str1,20,stdin);
-	int flag=0;
-	if(strlen(str1)!=strlen(str))
+	if(strcmp(str,str1)==0)
 	{
-		printf("It is not an anagram.\n");
+		printf("Anagram!\n");
 	}
 	else
 	{
-		for(int i=0;i<strlen(str);i++)
-		{
-			flag=0;
-			for(int j=0;j<strlen(str);j++)
-			{	flag=0;
-				if(str[i]==str1[j])
-				{
-					flag=1;
-					break;
-				}
-			}
-			if (flag==0)
-			{
-				printf("it is not an anagram.\n");
-				break;
-			}
-		}
+		printf("Not an anagram!\n");
 	}
-	if(flag==1)
-		printf("It is an anagram!\n");
 
 	return 0;
 }
