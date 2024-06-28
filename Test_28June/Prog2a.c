19a3fac8ad992c3dc33582083649d09021524acf
ayushisrivas <user13@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-06-28 11:56:56 +0000

    Test

--- Test_28June/Prog2a.c
+++ Test_28June/Prog2a.c
@@ -6,16 +6,21 @@
 int removchar(char a[],int k)
 {
 	char s=a[k];
-	int count=1;
+	int count=0;
 	for(int i=k+1;i<strlen(a);i++)
 	{
 		if(a[i]==s)
 			count++;
+		else 
+			break;
 
 	}
 	for(int i=k;i<strlen(a);i++)
 	{
-		a[i]=a[i+count];
+		printf("\n%c",a[i]);
+		a[i]=a[i+count+1];
+		printf("\n%c",a[i]);
+
 	}
 	return count;
 
@@ -31,14 +36,12 @@ int main()
 	int n=strlen(str);
 	for(int i=0;i<=n-1;i++)
 	{
-		if(strlen(str)<3)
-			break;
 		if((str[i])==str[i+1])
 		{
 			int f= removchar(str,i);
 			n-=f;
+			printf("\n%s, %c, %c\n",str,str[i],str[i+1]);
 			i=-1;
-	//		printf("\n%s, %c, %c\n",str,str[i],str[i+1]);
 		}
 	}
 	printf("The updated string is: ");
